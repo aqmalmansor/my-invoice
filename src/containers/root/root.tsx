c905bd430823b5bdfe507a495a98b0123845f59e
aqmalmansor <kifasu@gmail.com>
2024-12-31 16:32:47 +0800

    added radix

---
+++ src/containers/root/root.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+import { Theme } from "@radix-ui/themes";
+
+export const Root: FC = () => {
+  return <Theme>Root</Theme>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "@radix-ui/themes/styles.css";
+
+import { Root } from "./containers/root";
+
+import "./assets/styles/index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Root />
+  </StrictMode>
+);

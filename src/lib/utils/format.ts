be9d19c453dd38d3b50da95e3cd09ff4b9bf1e66
aqmalmansor <kifasu@gmail.com>
2025-01-02 01:02:05 +0800

    Enhanced curency

--- src/lib/utils/format.ts
+++ src/lib/utils/format.ts
@@ -1,7 +1,16 @@
-export const formatCurrency = (value: number, currency: string): string =>
-  currency +
-  " " +
-  parseFloat(value.toString()).toLocaleString("en-US", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
+import { currencies } from "../constants";
+
+export const formatCurrency = (value: number, currency: string): string => {
+  const currLabel = currencies
+    .find((c) => c.value === currency)
+    ?.label.match(/\((.*?)\)/);
+
+  return (
+    (currLabel ? currLabel[1] : "RM") +
+    " " +
+    parseFloat(value.toString()).toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })
+  );
+};

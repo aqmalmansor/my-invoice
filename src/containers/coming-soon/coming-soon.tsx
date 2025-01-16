f3c5809d14ca60dac123a293f925184074588c7d
aqmalmansor <kifasu@gmail.com>
2025-01-16 09:14:15 +0800

    added overflow and updated copywriting

--- src/containers/coming-soon/coming-soon.tsx
+++ src/containers/coming-soon/coming-soon.tsx
@@ -4,7 +4,7 @@ import { Text } from "@radix-ui/themes";
 export const ComingSoon: FC = () => (
   <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
     <div className="w-full bg-red flex flex-col items-center justify-center h-full gap-2">
-      <Text size="8">Please come back later</Text>
+      <Text size="8">Coming soon</Text>
     </div>
   </div>
 );

a02b0d02c3c783816d6768b51a5dece829fdbe30
aqmalmansor <kifasu@gmail.com>
2025-01-30 14:16:30 +0800

    refactored

--- src/containers/invoice/form-fields/preview-pdf-modal/pdf/sections/invoice-header.tsx
+++ src/containers/invoice/form-fields/preview-pdf-modal/pdf/sections/invoice-header.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
 import { Image, Text, View } from "@react-pdf/renderer";
 
+import { FormValuesType } from "@app/containers/invoice";
+import { DEBUG } from "@app/lib/constants";
 import { formatDate } from "@app/lib/utils";
 
-import { FormValuesType } from "../../../../config";
 import { invoiceHeaderStyles as styles } from "./config";
 
 type InvoiceHeaderProps = Omit<
@@ -23,7 +24,7 @@ export const InvoiceHeader: FC<InvoiceHeaderProps> = ({
       <View style={styles.container}>
         <View style={[styles.column, styles.logoCol]}>
           {business.logo && (
-            <Image src={business.logo} style={styles.logo} debug />
+            <Image src={business.logo} style={styles.logo} debug={DEBUG} />
           )}
         </View>
         <View style={[styles.column, styles.rightColBasis, { gap: "10px" }]}>
